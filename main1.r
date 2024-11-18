10165774aedd85a04831d94aac998b5bf0aa831e
M E Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2024-11-18 15:47:42 +0600

    introduction to R

---
+++ main1.r
@@ -0,0 +1,15 @@
+#Introduction
+
+# To install R on your Windows, just follow these steps:
+
+# 1.Install VS Code
+# 2.Download the R Installer File
+# 3.Run the Installer
+# 4.Install R
+# 5.Add R to the PATH Environment Variable Manually
+# 6.Verify the Installation
+
+
+greet<- 'Hello Friend'
+
+print(greet)
